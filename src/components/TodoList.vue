e375c77d1f3d521d7a223576c797ad868dc1b615
Artem <31787423+DokSna@users.noreply.github.com>
2021-01-19 13:54:07 +0300

    add Todoitem.vue

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
     <ul>
-      <li>todo 1</li>
+      <!-- <li>todo 1</li> -->
+      <Todoitem />
     </ul>
   </div>
 </template>
+
+<script>
+import Todoitem from '@/components/Todoitem';
+export default {
+  components: {
+    Todoitem,
+  },
+};
+</script>
